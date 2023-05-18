4706f6bb24667f36cce7f73a0568a6ed0734877f
Nitin Londhe <nitin.londhe@genmills.com>
2023-05-18 17:31:28 +0530

    added movie repository

---
+++ Movies.Application/AppRegistry/ApplicationServiceCollectionExtentions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Movies.Application.Repositories;
+
+namespace Movies.Application.AppRegistry;
+
+public static class ApplicationServiceCollectionExtentions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMovieRepository, MovieRepository>();
+
+        return services;
+    }
+}
---
+++ Movies.Application/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace Movies.Application.Models;
+
+public record Movie
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+    public required int YearOfRelease { get; init; }
+    public required List<string> Genres { get; init; } = new();
+}
